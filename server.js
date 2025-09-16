70a7deb22d247ef3abb2732ececc73fd554007e8
rivestudy <galang0559@gmail.com>
2025-09-16 13:34:02 +0700

    pre-1.0.0.5

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import missionRoutes from './routes/missions.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
